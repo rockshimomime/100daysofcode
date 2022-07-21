4711f115ef6eb9f2e76cf3d1979cd27eb603b7e0
Rock <sh.alencar@rocketmail.com>
2022-07-21 16:43:23 -0300

    feat(Day-1): create a band's name generator program

---
+++ Day 1/band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator!")
+city = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet name?\n")
+print(f"Your band's name could be: {city} {pet_name}")
